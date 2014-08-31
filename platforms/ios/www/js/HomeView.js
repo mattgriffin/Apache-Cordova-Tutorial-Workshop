828b88d61b50ef6971f7d3ba177e8da067483688
Matt Griffin <me@mattgriffin.com>
2014-08-31 13:29:52 -0500

    Completed tutorial
    From http://ccoenraets.github.io/cordova-tutorial/index.html

---
+++ platforms/ios/www/js/HomeView.js
@@ -0,0 +1,33 @@
+var HomeView = function (service) {
+
+	var employeeListView;
+
+	this.initialize = function() {
+		this.$el = $('<div/>');
+		this.$el.on('keyup', '.search-key', this.findByName);
+		employeeListView = new EmployeeListView();
+		this.render();
+	};
+
+	this.render = function() {
+		this.$el.html(this.template());
+		$('.content', this.$el).html(employeeListView.$el);
+		this.findAll();
+		return this;
+	};
+
+	this.findByName = function() {
+		service.findByName($('.search-key').val()).done(function(employees) {
+			employeeListView.setEmployees(employees);
+		});
+	};
+
+	this.findAll = function() {
+		service.findByName('').done(function(employees) {
+			employeeListView.setEmployees(employees);
+		});
+	};
+
+	this.initialize();
+
+}
